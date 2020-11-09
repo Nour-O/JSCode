a140b3909b8fa3413527201a8da9d85385b66a15
Nour-O <74199177+Nour-O@users.noreply.github.com>
2020-11-09 17:01:48 +0100

    Add files via upload

---
+++ StautsRedTitleScript.js
@@ -0,0 +1,5 @@
+$(document).ready(function() 
+{ var y = document.getElementsByClassName("confluence-information-macro-warning");
+  if(y.length > 0) { 
+	document.getElementById('title-text').getElementsByTagName('a')[0].style.color = "#bf2600"; } 
+});
