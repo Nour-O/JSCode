9291e9709b4af48c424c4272fa50ec402dab0724
Nour-O <74199177+Nour-O@users.noreply.github.com>
2020-11-10 12:33:02 +0100

    Update StautsRedTitleScript.js

--- StautsRedTitleScript.js
+++ StautsRedTitleScript.js
@@ -1,5 +1,7 @@
+alert("Hi");
 $(document).ready(function() 
-{ var y = document.getElementsByClassName("confluence-information-macro-warning");
+{alert("Hi inside");
+	var y = document.getElementsByClassName("confluence-information-macro-warning");
   if(y.length > 0) { 
 	document.getElementById('title-text').getElementsByTagName('a')[0].style.color = "#bf2600"; } 
 });
